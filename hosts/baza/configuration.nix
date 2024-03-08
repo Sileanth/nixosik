2cfc15e71a9b8ab7b5eeaa6a96baefaf6b4774ec
sileanth <lukasz.magnuszewski@gmail.com>
2024-03-08 06:24:24 +0100

    upodate and kde fixing

--- hosts/baza/configuration.nix
+++ hosts/baza/configuration.nix
@@ -103,7 +103,7 @@
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.enable = false;
   services.xserver.displayManager.autoLogin.user = "sileanth";
 
   # Allow unfree packages
