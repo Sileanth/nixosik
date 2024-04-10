836540b0ede221db011e64b15df99a3022d3856e
sileanth <lukasz.magnuszewski@gmail.com>
2024-04-10 11:22:41 +0200

    ipv6

--- modules/nixos/postgres.nix
+++ modules/nixos/postgres.nix
@@ -14,7 +14,7 @@
       authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
         host  all      all     127.0.0.1/32   trust
-        host   all             all             fe80::/16    trust
+            host all       all     ::1/128        trust
         local all      all     trust
     '';
    };
