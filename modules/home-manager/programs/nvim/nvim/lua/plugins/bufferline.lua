c0344fa6bac82bc30e4533261516e7856e35fcd9
sileanth <lukasz.magnuszewski@gmail.com>
2024-04-12 08:39:57 +0200

    nvim

--- modules/home-manager/programs/nvim/nvim/lua/plugins/bufferline.lua
+++ modules/home-manager/programs/nvim/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,9 @@
 return {
   'akinsho/bufferline.nvim',
   dependencies = {
-    require("bufferline").setup {}
-  }
+    'nvim-tree/nvim-web-devicons'
+  },
+  config = function ()
+        require("bufferline").setup {}
+  end
 }
