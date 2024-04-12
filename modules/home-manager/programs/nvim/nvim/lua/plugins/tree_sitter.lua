2ffbebce5b3147dbc2d1ba6acd03a9ee9857313d
sileanth <lukasz.magnuszewski@gmail.com>
2024-04-12 08:54:34 +0200

    nvim

--- modules/home-manager/programs/nvim/nvim/lua/plugins/tree_sitter.lua
+++ modules/home-manager/programs/nvim/nvim/lua/plugins/tree_sitter.lua
@@ -4,7 +4,7 @@ return {
 
 
   config = function()
-    require'nvim-treesitter.configs'.setup {
+    require 'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
@@ -29,12 +29,12 @@ return {
         -- list of language that will be disabled
         -- disable = { "latex" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
         end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -44,7 +44,6 @@ return {
         additional_vim_regex_highlighting = false,
       },
     }
-
   end
 
 
