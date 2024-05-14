369778ce20b10256fbe7fe5aa810c3496bed2f24
sileanth <lukasz.magnuszewski@gmail.com>
2024-05-14 09:34:10 +0200

    wayland

--- hosts/baza/configuration.nix
+++ hosts/baza/configuration.nix
@@ -113,6 +113,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wayland
+    waylandpp
     wget
   ];
 
