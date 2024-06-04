f69671037d461e385875bf835d7fd87e7337d363
Your Name <you@example.com>
2024-06-04 18:09:19 +0200

    fix warnings

--- hosts/delik/configuration.nix
+++ hosts/delik/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  config,
+  # config,
   pkgs,
   outputs,
   ...
@@ -115,6 +115,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    vim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
