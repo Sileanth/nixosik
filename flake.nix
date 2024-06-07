86ba9d18d4e027be2ab53ebf928439785cf5aa87
sileanth <lukasz.magnuszewski@gmail.com>
2024-06-07 16:52:53 +0200

    intial dekstop setup

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     # Nixpkgs
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -11,7 +10,7 @@
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
 
@@ -84,6 +83,15 @@
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
+	biurko = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules =
+          base.modules
+          ++ [
+            # > Our main nixos configuration file <
+            ./hosts/biurko/configuration.nix
+          ];
+      };
       inspir = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules =
@@ -119,6 +127,16 @@
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       "sileanth@inspir" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+          inputs.plasma-manager.homeManagerModules.plasma-manager
+        ];
+      };
+      "sileanth@biurko" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
 
