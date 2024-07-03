e630501acdc4b4fb3123d8be7ca974d44646a18f
sileanth <lukasz.magnuszewski@gmail.com>
2024-07-03 12:13:07 +0200

    hartime is annoying

--- modules/home-manager/programs/nvim/nvim/lua/plugins/learn.lua
+++ modules/home-manager/programs/nvim/nvim/lua/plugins/learn.lua
@@ -26,10 +26,10 @@ return {
       -- },
     },
   },
-  {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {}
-  }
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   opts = {}
+  -- }
 
 }
