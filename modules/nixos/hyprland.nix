e95a55df9e3d131fb28a378d6dcdf5a640411527
sileanth <lukasz.magnuszewski@gmail.com>
2024-11-20 11:17:22 +0100

    hyprland added portal from flake

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -11,7 +11,8 @@
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   # fix invisible cursor
