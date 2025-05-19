31e3d46744b941819cf2a490e34c1a5a899e34b9
sileanth <lukasz.magnuszewski@gmail.com>
2025-05-19 09:32:56 +0200

    deleted save config(unsupported with flakes)

--- hosts/athena/configuration.nix
+++ hosts/athena/configuration.nix
@@ -72,11 +72,6 @@
   services.openssh.enable = true;
 
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
   system.stateVersion = "24.11"; 
 
 }
