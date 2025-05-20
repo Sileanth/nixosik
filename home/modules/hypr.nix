42e34d21d9e5332ea17a7285d96a3a81fc51573a
sileanth <lukasz.magnuszewski@gmail.com>
2025-05-20 17:04:12 +0200

    keybind change

--- home/modules/hypr.nix
+++ home/modules/hypr.nix
@@ -236,7 +236,7 @@ settings = {
 	"$mod, 7, workspace, 7"
 	"$mod, 8, workspace, 8"
 	"$mod, 9, workspace, 9"
-	"$mod, 10, workspace, 10"
+	"$mod, 0, workspace, 10"
 
 	"$mod SHIFT, 1, movetoworkspace, 1"
 	"$mod SHIFT, 2, movetoworkspace, 2"
@@ -247,7 +247,7 @@ settings = {
 	"$mod SHIFT, 7, movetoworkspace, 7"
 	"$mod SHIFT, 8, movetoworkspace, 8"
 	"$mod SHIFT, 9, movetoworkspace, 9"
-	"$mod SHIFT, 10, movetoworkspace, 10"
+	"$mod SHIFT, 0, movetoworkspace, 10"
 		
 
 
