848dcd535578b194d002ea3972e79da69fcac2f3
sileanth <lukasz.magnuszewski@gmail.com>
2025-05-22 14:35:36 +0000

    svelte lsp nvim

--- home/modules/nvim/nvim/lua/plugins/lspconfig.lua
+++ home/modules/nvim/nvim/lua/plugins/lspconfig.lua
@@ -7,6 +7,7 @@ return {
 		vim.lsp.enable('prolog_ls')
 		vim.lsp.enable('ts_ls')
 		vim.lsp.enable('csharp_ls')
+    vim.lsp.enable('svelte')
 		vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 
 
