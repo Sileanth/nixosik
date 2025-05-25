19e608ac8bab92d9bda598fe71d2d2738e25a353
sileanth <lukasz.magnuszewski@gmail.com>
2025-05-25 08:43:50 +0000

    bootgloader module fixed

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -1,5 +1,9 @@
 {lib, config, pkgs, ...}: let
 	cfg = config.mc.bootloader;
+  mkIfElse = p: yes: no: lib.mkMerge [
+    (lib.mkIf p yes)
+    (lib.mkIf (!p) no)
+  ];
 in {
 
 imports = [
@@ -18,45 +22,42 @@ options = {
 
 
 };
-config = {
-  lib.mkIf (cfg.type == "grub") {
-    boot.loader.grub.enable = true;
-    boot.loader.grub.default = saved;
-    boot.loader.grub.efiSupport = true;
-    boot.loader.grub.efiInstallAsRemovable = true;
-    extraEntries =
-        (if config.mc.windows.enable then
-        ''
-          menuentry "Windows" {
-            insmod part_gpt
-            insmod fat
-            insmod search_fs_uuid
-            insmod chain
-            search --fs-uuid --set=root 7C30-1BC2
-            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-          }
-        '' else "")
-        + ''
-           menuentry "Reboot" {
-              reboot
-          }
+config = 
+  let 
+    grubConfig = {
+      boot.loader.grub.enable = true;
+      boot.loader.grub.default = "saved";
+      boot.loader.grub.efiSupport = true;
+      boot.loader.grub.efiInstallAsRemovable = true;
+        boot.loader.grub.device = "nodev";
+      boot.loader.grub.extraEntries =
+          ''
+            menuentry "Windows" {
+              insmod part_gpt
+              insmod fat
+              insmod search_fs_uuid
+              insmod chain
+              search --fs-uuid --set=root 7C30-1BC2
+              chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+            }
+          '' 
+          + ''
+             menuentry "Reboot" {
+                reboot
+            }
 
-          menuentry "Shut Down" {
-              halt
-          }
-        '';
-
-  };
-
-  lib.mkIf (cfg.type == "systemdboot") {
-
-    # Use the systemd-boot EFI boot loader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-  };
+            menuentry "Shut Down" {
+                halt
+            }
+          '';
+    };
+    systemdConfig = {
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+    };
+in 
+  mkIfElse (cfg.type == "grub") grubConfig systemdConfig;
 
 
-};
 }
 
