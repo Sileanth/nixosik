55b86ad79fe927c6f0292b7b985a49e63a4af99c
sileanth <lukasz.magnuszewski@gmail.com>
2025-05-26 12:00:09 +0000

    migrate back to oil, and add keybind

---
+++ home/modules/nvim/nvim/lua/plugins/oil.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    vim.keymap.set('n', '<leader>o', function()
+      vim.cmd('Oil --')
+    end, { desc = 'Open Oil (file explorer' }
+
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    require("oil").setup(opts)
+
+
+  end
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
+
