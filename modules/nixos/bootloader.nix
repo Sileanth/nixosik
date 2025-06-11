a400c434d91a3de4bd1bfa1e31a46332069a6d0b
sileanth <lukasz.magnuszewski@gmail.com>
2025-06-12 00:17:31 +0200

    hollow grub

--- modules/nixos/bootloader.nix
+++ modules/nixos/bootloader.nix
@@ -4,6 +4,19 @@
     (lib.mkIf p yes)
     (lib.mkIf (!p) no)
   ];
+  hollow-grub = pkgs.stdenv.mkDerivation {
+    name = "hollow-grub";
+    src = pkgs.fetchFromGitHub {
+      owner = "sergoncano";
+        repo = "hollow-knight-grub-theme";
+        rev = "7ae57b6";
+        sha256 = "sha256-tV87/7Gc+LpRvSMpgWJ+x/qrW2cbR/3pmoZA3TT/JHU=";
+    };
+    installPhase = ''
+      mkdir -p $out/grub/themes/hollow-grub
+      cp -rp hollow-grub $out/grub/themes
+    '';
+  };
 in {
 
 imports = [
@@ -27,6 +40,7 @@ config =
     grubConfig = {
       boot.loader.grub.enable = true;
       boot.loader.grub.default = "saved";
+      boot.loader.grub.theme = "${hollow-grub}/grub/themes/hollow-grub";
       boot.loader.grub.efiSupport = true;
       boot.loader.grub.efiInstallAsRemovable = true;
         boot.loader.grub.device = "nodev";
