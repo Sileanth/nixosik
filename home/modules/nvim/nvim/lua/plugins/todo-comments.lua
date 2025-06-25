fc54a3dd325753a2cb6d89cbb3cab848d96ef29c
sileanth <lukasz.magnuszewski@gmail.com>
2025-06-25 12:02:31 +0200

    nvim todo comments

--- home/modules/nvim/nvim/lua/plugins/todo-comments.lua
+++ home/modules/nvim/nvim/lua/plugins/todo-comments.lua
@@ -5,5 +5,8 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-  }
+  },
+  keys = {
+    { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+  },
 }
