18a44e837f07e2812e76fced1a9081f1cf59278d
sileanth <lukasz.magnuszewski@gmail.com>
2025-10-15 18:30:17 +0200

    rust nvim

--- home/modules/nvim/nvim/lua/plugins/lspconfig.lua
+++ home/modules/nvim/nvim/lua/plugins/lspconfig.lua
@@ -8,6 +8,7 @@ return {
 		vim.lsp.enable('ts_ls')
 		vim.lsp.enable('csharp_ls')
 		vim.lsp.enable('gopls')
+    vim.lsp.enable('rust_analyzer')
 
     vim.lsp.enable('svelte')
     vim.lsp.enable('astro')
