216561b23a16650d52477ec8eea0fad9308c9afa
sileanth <sileanth@sileanth.pl>
2025-12-27 12:53:07 +0100

    swswitch dns from isp to cloudflare/google

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     neovim.url = "github:nix-community/neovim-nightly-overlay";
     nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-25.11";
-      # url = "github:nixos/nixpkgs/nixos-unstable";
+      # url = "github:nixos/nixpkgs/nixos-25.11";
+      url = "github:nixos/nixpkgs/nixos-unstable";
     };
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
    
 
     disko = {
--- hosts/helios/configuration.nix
+++ hosts/helios/configuration.nix
@@ -16,6 +16,7 @@
   ];
 
   networking.hostName = "helios"; # Define your hostname.
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
